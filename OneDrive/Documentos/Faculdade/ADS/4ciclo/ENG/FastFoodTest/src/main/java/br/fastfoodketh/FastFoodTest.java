b47145a29a16df0c0c6ca7dff034d3f5e8f36022
Kethleen <kethleen_rosa@outlook.com>
2025-04-24 20:42:33 -0300

    Fast Food da Keth

---
+++ OneDrive/Documentos/Faculdade/ADS/4ciclo/ENG/FastFoodTest/src/main/java/br/fastfoodketh/FastFoodTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.fastfoodketh;
+
+/**
+ *
+ * @author kethe
+ */
+public class FastFoodTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
